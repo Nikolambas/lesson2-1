bbfe36bfc8606e4f4da17e7ba2be79ad842f01dc
Nikolambas <88347156+Nikolambas@users.noreply.github.com>
2021-10-20 23:55:27 +0300

    Update Wall.java

--- Wall.java
+++ Wall.java
@@ -1,18 +1,1 @@
-package com.company;
 
-public class Wall {
-    private int height=3;
-
-    public int getHeight() {
-        return height;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (obj instanceof Jumpable){
-            return ((Jumpable) obj).jump();
-        }
-        System.out.println("Обьект не умеет прыгать");
-        return false;
-    }
-}
