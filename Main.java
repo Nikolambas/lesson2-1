160c14ef16c60273773d3ec764c8d395876a7c1f
Nikolambas <88347156+Nikolambas@users.noreply.github.com>
2021-10-20 23:53:39 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,14 +1,1 @@
-package com.company;
 
-import java.util.ArrayList;
-
-public class Main {
-
-   public static void main(String[] args) {
-        Team team = new Team(new Robot(),new Cat(),new Man(),new Robot());
-        Course course = new Course(new Treadmill(),new Wall());
-        team.printTeam(team.getTeam());
-        course.dolt(team);
-        team.showResults();
-    }
-}
